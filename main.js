e0d0b8825ccd239c7afe05fce613d6501480dd83
Tammys-website <85290513+Tammys-website@users.noreply.github.com>
2021-08-19 19:50:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name="Tammanna";
+var last_name = "jhawar";
+
+ console.log(first_name);
+ console.log(first_name+" "+last_name);
+
+ var length = first_name.length;
+ console.log (length);
+ var u = first_name.toUpperCase();
+console.log (u);
+var full_name = first_name.concat(last_name);
+console.log (full_name);
+
+var r = first_name.replace("Tammanna","Tammy");
+console.log (r);
